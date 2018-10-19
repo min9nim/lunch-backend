6e73a5d7635e67c17377790b5db23e581dcbdad7
songmingu(mac) <min9nim@gmail.com>
2018-10-19 15:37:19 +0900

    정적리소스 추가

---
+++ public/precache-manifest.afeda3154bcad9f2e9a4c7e0f98bb5c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0515c5c5a2bad9b3ca8f",
+    "url": "/static/js/main.0515c5c5.chunk.js"
+  },
+  {
+    "revision": "037f9b5637870bad40ff",
+    "url": "/static/js/1.037f9b56.chunk.js"
+  },
+  {
+    "revision": "0515c5c5a2bad9b3ca8f",
+    "url": "/static/css/main.806621c2.chunk.css"
+  },
+  {
+    "revision": "0b663ce999ad3591c2ddbb902ef3db4b",
+    "url": "/index.html"
+  }
+];
